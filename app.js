083f964215cf83766e556fdd0c62b80e1c00f4e2
elilr22 <igju06@hotmail.com>
2025-02-03 22:01:09 -0300

    segundo commit tarea resuelta

--- app.js
+++ app.js
@@ -1,27 +1,133 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigosLista = []
+/* let amigosLista = []
 function borrarInput() {
-    let inputHTML = document.getElementById("amigo").value = ''
-}
+    document.getElementById("amigo").value = ''
+} */
 
-function agregarAmigo() {
+/* function agregarAmigo() {
     let inputHTML = document.getElementById("amigo")
-    let ulHTML = document.getElementById("listaAmigos")
     let value = inputHTML.value
 
     if (value == '') {
         alert('ingrese un valor')
     } else {
         amigosLista.push(value)
-        console.log(value);
         borrarInput()
         console.log(amigosLista);
-        ulHTML.innerHTML += `<li> ${amigosLista.pop()} </li>`
-        
+        mostrarArray()
     }
+} */
+
+/* 
+
+function agregarAmigo() {
+    let inputHTML = document.getElementById('amigo').value
+    console.log(inputHTML);
+
+    if (inputHTML == '') {
+        alert('ingrese un valor')
+    } else {
+        amigosLista.push(inputHTML)
+        console.log(amigosLista);
+    }
+    return
+}
+
+
+
+
+
+function mostrarArray() {
+    for (let i = 0; i < amigosLista.length; i++) {
+        console.log(amigosLista[i]);
+        let ulHTML = document.getElementById("listaAmigos")
+        ulHTML.innerHTML += `<li> ${amigosLista.pop()} </li>`
+    }
+    return
+}
+
+
+
+
+
+
+
+
+
+
+for (let lista of amigosLista) {
 
 
 }
+
+
+
+function sortearAmigo() {
+    if (amigosLista.length === 0) {
+        alert('ningun nombre en la lista')
+    }
+    else { alert('ganaste') }
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* 
 let ulHTML = document.getElementById("listaAmigos")
 ulHTML.innerHTML += `<li> ${amigosLista} </li>` */
+
+
+
+let amigosLista = []
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById('amigo');
+    let nombre = inputAmigo.value.trim()
+
+    if(nombre === ''){
+        alert('agregar un valor')
+        return
+    }
+    amigosLista.push(nombre)
+
+    inputAmigo.value = ''
+    console.log(amigosLista);
+    actualizarLista()
+    
+}
+
+
+function actualizarLista(){
+    let listaHTML = document.getElementById('listaAmigos')
+    listaHTML.innerHTML = ''
+
+    for(let i = 0; i < amigosLista.length; i++){
+        listaHTML.innerHTML += `<li>${amigosLista[i]}</li>`
+    }
+}
+
+
+
+function sortearAmigo(){
+    let resultado = document.getElementById("resultado")
+
+    if(amigosLista.length === 0){
+        alert('no pusiste ningun nombre en la lista de amigos');
+        return;
+    }
+    let numeroAleatorio = Math.floor(Math.random() * amigosLista.length);
+    let amigoSorteado = amigosLista[numeroAleatorio];
+    resultado.innerHTML = `<li> ¡El ganador es: <strong>${amigoSorteado}</strong>! </li>`;
+}
